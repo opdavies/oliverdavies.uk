face5a6e637128501f368683298d3c896a1ed9d3
Oliver Davies <oliver@oliverdavies.co.uk>
2015-03-22 09:56:41 +0000

    Adjusted SASS settings

--- source/themes/opdavies/opdavies/config.rb
+++ source/themes/opdavies/opdavies/config.rb
@@ -6,14 +6,6 @@
 ## file for more information.
 ##
 
-# Default to development if environment is not set.
-saved = environment
-if (environment.nil?)
-  environment = :development
-else
-  environment = saved
-end
-
 # Location of the theme's resources.
 css_dir = "assets/css"
 sass_dir = "assets/scss"
@@ -31,7 +23,7 @@ require 'sass-globbing'
 
 # You can select your preferred output style here (:expanded, :nested, :compact
 # or :compressed).
-output_style = (environment == :production) ? :expanded : :nested
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
@@ -39,7 +31,4 @@ output_style = (environment == :production) ? :expanded : :nested
 relative_assets = true
 
 # Conditionally enable line comments when in development mode.
-line_comments = (environment == :production) ? false : true
-
-# Output debugging info in development mode.
-sass_options = (environment == :production) ? {} : {:debug_info => true}
+line_comments = false
