ea5bcd6cb301c7db6882448d35ac7c5a420c5a1e
Oliver Davies <oliver@oliverdavies.co.uk>
2015-04-09 17:15:05 +0100

    Removed some empty lines

--- build.sh
+++ build.sh
@@ -17,13 +17,9 @@ rm -rf ${BUILD_DIR}
 git clone ${REPO} ${BUILD_DIR}
 
 pushd ${BUILD_DIR}
-
 git checkout -B ${BRANCH}
-
 rsync --quiet --archive --filter="P .git*" --delete ../output_${ENV}/ ./
-
 git add -A .
 git commit -m "${LOG}"
 git push origin ${BRANCH} --force
-
 popd
