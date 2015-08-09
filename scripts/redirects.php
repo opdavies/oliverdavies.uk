bbd1a56d58c1f52de28c7d2721a718d7fd766ddb
Oliver Davies <oliver@oliverdavies.uk>
2015-08-09 20:51:57 +0100

    Use the new filename

--- scripts/redirects.php
+++ scripts/redirects.php
@@ -25,8 +25,8 @@ if (($handle = fopen($csv, 'r')) !== FALSE) {
             $output = strtr($template, $templateData);
 
             $filename = str_replace('/', '-', $data[0]);
-            file_put_contents("source/{$data[0]}.html", $output);
-            echo "Written to {$data[0]}.html\n";
+            file_put_contents("source/$filename.html", $output);
+            echo "Written to $filename.html\n";
         }
     }
 }
