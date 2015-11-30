101a7669ebaf16db18eec4b49892f219515c6392
Oliver Davies <oliver@oliverdavies.uk>
2015-11-30 23:10:03 +0000

    Added publish and watch scripts

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./vendor/bin/sculpin generate --env=prod --clean --no-interaction
+if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
+
+rsync -av output_prod/ deployment@oliverdavies.uk:/var/www/vhosts/oliverdavies/public
+if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
