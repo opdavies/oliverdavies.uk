19eb9bd349434ec3945bc661de0ad4da3aba78ad
Oliver Davies <oliver@oliverdavies.uk>
2016-01-04 11:38:34 +0000

    Minified images

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('image', function () {
+    // Minify images.
     gulp.src('./source/assets/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./source/assets/images'));
