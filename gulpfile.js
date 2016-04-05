32f727ee5ff07eced6c50a629b1eae08ddd72a76
Oliver Davies <oliver@oliverdavies.uk>
2016-04-05 20:38:55 +0100

    Fixed path in scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('scripts', function () {
     app.js([
         config.bowerDir + '/jquery/dist/jquery.js',
         config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js',
-        config.assetsDir + config.jsPattern
+        config.assetsDir + '/' + config.jsPattern
     ], 'site.js');
 })
 
