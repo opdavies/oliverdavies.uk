e1c847867937b724de7625e6e5cd5c1c312642a5
Oliver Davies <oliver@oliverdavies.uk>
2017-01-08 02:11:19 +0000

    Update task names

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,24 @@
 from fabric.api import *
 from fabric.contrib.project import rsync_project
 
-env.hosts = ['production']
 env.use_ssh_config = True
 
 project_root = '/var/www/oliverdavies'
 
 @task
 def deploy():
-    build_assets()
-    build_site()
-    deploy_site()
-    fix_file_permissions()
+    build()
+    deploy()
+    file_permissions()
 
-def build_site():
+def build():
     local('composer install --no-dev --optimize-autoloader')
     local('composer run production')
     local('echo %s > output_prod/version' % env.build_number)
-
-def build_assets():
     local('npm run init')
     local('npm run production')
 
-def deploy_site():
+def deploy():
     rsync_project(
         remote_dir='%s/web/' % project_root,
         local_dir='output_prod/',
@@ -31,6 +27,6 @@ def deploy_site():
     )
     run('sudo service nginx configtest && sudo service nginx reload')
 
-def fix_file_permissions():
+def file_permissions():
     run('sudo chown -R %s:%s %s/web' % (env.user, env.group, project_root))
     run('sudo chmod -R 750 %s/web' % project_root)
