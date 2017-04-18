68847271e77d2e0da4602c93f52878ebf5d135af
Oliver Davies <oliver@oliverdavies.uk>
2017-04-18 19:03:18 +0100

    Revert "Revert "Change one command back to run""
    This reverts commit 7367b6efcb484e11f14c4f815d8a553e5edd5872.

--- fabfile.py
+++ fabfile.py
@@ -25,7 +25,7 @@ def deploy():
         default_opts='-vzcrSLh',
         delete=True
     )
-    sudo('service nginx configtest && service nginx reload')
+    run('sudo service nginx configtest && sudo service nginx reload')
 
 def file_permissions():
     run('sudo chown -R %s:%s %s/web' % (env.user, env.group, project_root))
