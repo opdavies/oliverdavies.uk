ecc9308ccb27950bad3e98a5baaba30cd5a7361b
Oliver Davies <oliver@oliverdavies.uk>
2017-07-25 18:00:13 +0100

    Ensure BUILD_DIR is defined and used

--- publish.sh
+++ publish.sh
@@ -5,29 +5,27 @@ set -e
 SITE_ENV="prod"
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-SOURCE_BRANCH="source"
-TARGET_BRANCH="master"
-BUILD_DIR=".build"
+BUILD_DIR="_deploy"
 
 # Prepare the build directory.
 git clone $REPO $BUILD_DIR
 pushd $BUILD_DIR
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git checkout master || git checkout --orphan master
 popd
 rm -rf ${BUILD_DIR}/**/* || exit 0
 
 # Re-generate the site.
 npm run prod
-vendor/bin/sculpin generate --no-interaction --clean --env=${SITE_ENV}
-touch output_${SITE_ENV}/.nojekyll
+vendor/bin/sculpin generate --no-interaction --clean --env=prod
+touch output_prod/.nojekyll
 
 # Add, commit and push the changes.
-mv output_${SITE_ENV}/* $BUILD_DIR
+mv output_prod/* $BUILD_DIR
 pushd $BUILD_DIR
-git add --all .
-git commit -m "Re-generate site: $SHA"
-git push $SSH_REPO $TARGET_BRANCH
+echo "!/assets/css/
+!/assets/images/
+!/assets/js/" > .gitignore
+git add --all -f .
+git commit -m "Re-generate site: `git rev-parse --verify HEAD`"
+git push $SSH_REPO master
 popd
-
-rm -rf $BUILD_DIR
