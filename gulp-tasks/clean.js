9ce0eaf00fe420277c7b40daccd55913e661ca80
Oliver Davies <oliver@oliverdavies.uk>
2017-08-03 02:15:09 +0100

    Wrap in a function, be consistent

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -1,6 +1,8 @@
 var del = require('del');
 
 module.exports = function (gulp, plugins, config) {
-    del.sync('source/assets/{css,fonts,js}');
-    del.sync('output_*/assets/{css,fonts,js}');
+    return function () {
+        del.sync('source/assets/{css,fonts,js}');
+        del.sync('output_*/assets/{css,fonts,js}');
+    }
 }
