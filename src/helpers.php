0774f465a239e8651f66c4338d020da777c8f0ba
Oliver Davies <oliver@oliverdavies.uk>
2018-08-31 00:35:25 +0100

    Replace custom tap function with josephlavin/tap

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,8 @@
 <?php
 
-function tap($value, $callback)
-{
-    $callback($value);
-
-    return $value;
-}
+//function tap($value, $callback)
+//{
+//    $callback($value);
+//
+//    return $value;
+//}
