c25710b75f76e23ebcfd42bcae69639fb95835b0
Oliver Davies <oliver@oliverdavies.uk>
2018-09-04 22:10:47 +0100

    Rename files

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,11 +4,11 @@ require('laravel-mix-purgecss');
 require('laravel-mix-tailwind');
 
 mix.disableNotifications()
-    .sass('assets/sass/site.sass', 'source/dist/css')
+    .sass('assets/sass/app.sass', 'source/dist/css')
     .combine([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/highlightjs/highlight.pack.js',
-        'assets/js/site.js',
+        'assets/js/app.js',
     ], 'source/dist/js/all.js')
     .copyDirectory('node_modules/font-awesome/fonts', 'source/dist/fonts')
     .tailwind()
