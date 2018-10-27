5a09dcdb19727159541c33e3222311c3a6dd8533
Oliver Davies <oliver@oliverdavies.uk>
2018-10-27 20:05:16 +0100

    Move CSS, JS and fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,13 +4,13 @@ require('laravel-mix-purgecss');
 require('laravel-mix-tailwind');
 
 mix.disableNotifications()
-    .postCss('assets/css/app.css', 'source/dist/css')
+    .postCss('assets/css/app.css', 'source/css')
     .js([
         // 'node_modules/highlightjs/highlight.pack.js',
-        'node_modules/jquery/dist/jquery.js',
+        'node_modules/jquery/src/jquery.js',
         'assets/js/app.js',
-    ], 'source/dist/js/all.js')
-    .copyDirectory('node_modules/font-awesome/fonts', 'source/dist/fonts')
+    ], 'source/js/all.js')
+    .copyDirectory('node_modules/font-awesome/fonts', 'source/fonts')
     .tailwind()
     .purgeCss({
         globs: [
