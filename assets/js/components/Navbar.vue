81b542581ee483b7b3e6c25246f80352380a5f84
Oliver Davies <oliver@microserve.io>
2018-12-15 20:44:13 +0000

    Add button label

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -8,7 +8,7 @@
                     </div>
 
                     <div class="w-1/4 text-right sm:hidden">
-                        <button type="button" class="nav-toggle appearance-none" @click="mobileNavHidden = !mobileNavHidden">
+                        <button type="button" class="nav-toggle appearance-none" @click="mobileNavHidden = !mobileNavHidden" aria-label="Toggle main menu">
                             <svg class="fill-current text-grey-dark w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z"/></svg>
                         </button>
                     </div>
