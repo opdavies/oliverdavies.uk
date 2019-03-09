5cf2312233c0be537cf964fd0afa5ee1af805ab1
Oliver Davies <oliver@oliverdavies.uk>
2019-03-09 08:15:13 +0000

    Swap sass for plain CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,22 @@
 let mix = require('laravel-mix');
 
 require('laravel-mix-purgecss');
-require('laravel-mix-tailwind');
 
 mix.disableNotifications()
-    .sass('assets/sass/app.sass', 'source/dist/css')
-    .js([
-        'node_modules/jquery/src/jquery.js',
-        'assets/js/app.js',
-    ], 'source/dist/js/all.js')
-    .copyDirectory('node_modules/font-awesome/fonts', 'source/dist/fonts')
-    .tailwind('tailwind.config.js')
-    .purgeCss({
-        globs: [
-            path.join(__dirname, 'assets/js/**/*.{js,vue}'),
-            path.join(__dirname, 'output_*/**/*.html'),
-        ],
-        whitelistPatterns: [/language/, /hljs/],
-        whitelistPatternsChildren: [/^markdown$/]
-    });
+  .postCss('assets/css/app.css', 'source/dist/css', [
+    require('postcss-nested'),
+    require('tailwindcss')('tailwind.config.js'),
+  ])
+  .js([
+    'node_modules/jquery/src/jquery.js',
+    'assets/js/app.js',
+  ], 'source/dist/js/all.js')
+  .copyDirectory('node_modules/font-awesome/fonts', 'source/dist/fonts')
+  .purgeCss({
+    globs: [
+      path.join(__dirname, 'assets/js/**/*.{js,vue}'),
+      path.join(__dirname, 'output_*/**/*.html'),
+    ],
+    whitelistPatterns: [/language/, /hljs/],
+    whitelistPatternsChildren: [/^markdown$/]
+  });
