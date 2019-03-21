5729a6b6c069c6ec30518159562270b015a6295b
Oliver Davies <oliver@oliverdavies.uk>
2019-03-21 09:37:48 +0000

    Use default Tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,6 @@ let defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   theme: {
     extend: {
-      colors: {
-        blue: {
-          dark: '#2779bd',
-          default: 'hsl(203, 94%, 38%)',
-          lighter: '#bcdefa',
-        },
-        grey: {
-          dark: '#70818a',
-          darker: '#596a73',
-          default: '#9babb4',
-          light: 'hsl(203, 25%, 88%)',
-          lighter: 'hsl(203, 22%, 95%)',
-          lightest: '#fafcfc',
-        },
-      },
       fontFamily: {
         mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
       },
