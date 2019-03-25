78ec052af8178a0b1f249a3c7e22aa9edb609c2a
Oliver Davies <oliver@oliverdavies.uk>
2019-03-25 15:40:43 +0000

    Split up sections in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,19 +3,22 @@ let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
 mix.disableNotifications()
-  .postCss('assets/css/app.css', 'source/dist/css', [
-    require('postcss-nested'),
-    require('tailwindcss')('tailwind.config.js'),
-  ])
-  .js([
-    'node_modules/jquery/src/jquery.js',
-    'assets/js/app.js',
-  ], 'source/dist/js/app.js')
-  .purgeCss({
-    globs: [
-      path.join(__dirname, 'assets/js/**/*.{js,vue}'),
-      path.join(__dirname, 'output_*/**/*.html'),
-    ],
-    whitelistPatterns: [/language/, /hljs/],
-    whitelistPatternsChildren: [/^markdown$/]
-  });
+
+mix.postCss('assets/css/app.css', 'source/dist/css', [
+  require('postcss-nested'),
+  require('tailwindcss')('tailwind.config.js'),
+])
+
+mix.js([
+  'node_modules/jquery/src/jquery.js',
+  'assets/js/app.js',
+], 'source/dist/js/app.js')
+
+mix.purgeCss({
+  globs: [
+    path.join(__dirname, 'assets/js/**/*.{js,vue}'),
+    path.join(__dirname, 'output_*/**/*.html'),
+  ],
+  whitelistPatterns: [/language/, /hljs/],
+  whitelistPatternsChildren: [/^markdown$/]
+});
