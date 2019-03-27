639e04b936e0e97babcf0d3d17eadc51634798a4
Oliver Davies <oliver@oliverdavies.uk>
2019-03-27 14:22:55 +0000

    Use the fluent API

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,24 +3,20 @@ let mix = require('laravel-mix')
 require('laravel-mix-purgecss')
 
 mix.disableNotifications()
-
-mix.postCss('assets/css/app.css', 'source/dist/css', [
-  require('postcss-nested'),
-  require('tailwindcss')('tailwind.config.js'),
-])
-
-mix.js('assets/js/app.js', 'source/dist/js')
-
-mix.browserSync({
-  proxy: 'localhost:8000',
-  files: [
-    'source/**/*.{html,md,twig}',
-  ],
-  notify: false,
-})
-
-mix.purgeCss({
-  folders: ['assets', 'output_*'],
-  whitelistPatterns: [/language/, /hljs/],
-  whitelistPatternsChildren: [/^markdown$/]
-})
+  .postCss('assets/css/app.css', 'source/dist/css', [
+    require('postcss-nested'),
+    require('tailwindcss')('tailwind.config.js'),
+  ])
+  .js('assets/js/app.js', 'source/dist/js')
+  .browserSync({
+    proxy: 'localhost:8000',
+    files: [
+      'source/**/*.{html,md,twig}',
+    ],
+    notify: false,
+  })
+  .purgeCss({
+    folders: ['assets', 'output_*'],
+    whitelistPatterns: [/language/, /hljs/],
+    whitelistPatternsChildren: [/^markdown$/]
+  })
