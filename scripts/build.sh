bb43ed81447ac46c2e1dda88723ab2c493acd170
Oliver Davies <oliver@oliverdavies.uk>
2019-04-09 20:10:29 +0100

    Remove scripts

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. scripts/common.sh
-
-$SCULPIN generate --clean --no-interaction $@
--- scripts/watch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. scripts/common.sh
-
-$SCULPIN generate --server --watch --clean --no-interaction $@
