abd4f6169e9809e56d876bfb875f4182b235b9ab
Oliver Davies <oliver@oliverdavies.uk>
2019-05-31 13:19:46 +0100

    Remove trailing commas

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        inherit: 'inherit',
+        inherit: 'inherit'
       },
       fontFamily: {
-        mono: ['Roboto Mono', ...fontFamily.mono],
+        mono: ['Roboto Mono', ...fontFamily.mono]
       },
       spacing: {
-        '2px': '2px',
+        '2px': '2px'
       },
       borderWidth: {
-        3: '3px',
+        3: '3px'
+      },
       width: {
         96: '24rem'
       }
