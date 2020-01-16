45ac4e0388d3a34ab8f5adcfe150269227f203fe
Oliver Davies <oliver@oliverdavies.uk>
2020-01-16 08:57:20 +0000

    Fix spaces in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ let PurgecssConfig = require('./purgecss.config')
 let PurgecssPlugin = require('purgecss-webpack-plugin')
 
 Encore
-  .disableSingleRuntimeChunk()
-  .cleanupOutputBeforeBuild()
-  .setOutputPath('source/build/')
-  .setPublicPath('/build')
-  .addEntry('app', './assets/js/app.js')
-  .enablePostCssLoader()
-  .enableSourceMaps(!Encore.isProduction())
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .setOutputPath('source/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .enablePostCssLoader()
+    .enableSourceMaps(!Encore.isProduction())
 
 if (Encore.isProduction()) {
-  Encore.addPlugin(new PurgecssPlugin(PurgecssConfig))
+    Encore.addPlugin(new PurgecssPlugin(PurgecssConfig))
 }
 
 module.exports = Encore.getWebpackConfig()
