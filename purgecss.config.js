62c1ee08d870c9560c48987bf53fbd9cf9bb2299
Oliver Davies <oliver@oliverdavies.uk>
2020-02-05 22:53:33 +0000

    Fix syntax highlighting

--- purgecss.config.js
+++ purgecss.config.js
@@ -4,5 +4,6 @@ module.exports = {
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
   paths: () => glob.sync([
     'source/**/*.{md,twig}'
-  ])
+  ]),
+  whitelistPatterns: [/hljs/, /language/]
 }
