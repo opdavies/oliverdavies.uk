a3ceeaf0f3ab98ab897f5fc7b4533c7a7ff0aa1d
Oliver Davies <oliver@oliverdavies.uk>
2020-03-08 17:52:42 +0000

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf',
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
