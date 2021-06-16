15014f16ff0120015550a3489ac837543996bf1f
Oliver Davies <oliver@oliverdavies.uk>
2021-06-16 08:00:00 +0100

    Re-add Tailwind CSS

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+let Encore = require('@symfony/webpack-encore')
+let path = require('path')
+
+Encore
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .setOutputPath('source/build/')
+    .setPublicPath('/build')
+    .setManifestKeyPrefix('build/')
+
+    .addEntry('app', '@/app.js')
+
+    .enablePostCssLoader()
+
+    .addAliases({
+        '@': path.resolve(__dirname, 'assets', 'js'),
+        styles: path.resolve(__dirname, 'assets', 'css')
+    })
+
+if (!Encore.isProduction()) {
+    Encore.enableSourceMaps()
+}
+
+process.env.NODE_ENV =
+  process.env.NODE_ENV || Encore.isProduction() ? 'production' : 'development';
+
+module.exports = Encore.getWebpackConfig()
