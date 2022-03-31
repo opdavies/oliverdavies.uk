0ccf85bfdfb1aa3fade934681fefe599c91a1880
Shift <shift@laravelshift.com>
2022-03-31 16:40:52 +0000

    Configure JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,66 +3,45 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const { fontFamily } = defaultTheme
 
 module.exports = {
-  mode: 'jit',
-  darkMode: 'media',
-  important: true,
-  purge: {
-    content: [
-      'tools/tailwindcss/safelist-classes.txt',
-      'source/**/*.{md,twig}',
-    ],
-  },
+  content: ['tools/tailwindcss/safelist-classes.txt', 'source/**/*.{md,twig}'],
   theme: {
     outline: {
       black: '1px solid black',
-      white: '1px solid white'
+      white: '1px solid white',
     },
     extend: {
       typography: (theme) => ({
         DEFAULT: {
           css: {
             p: {
-              lineHeight: 400
-            }
-          }
-        }
+              lineHeight: 400,
+            },
+          },
+        },
       }),
-      colors,
       fontFamily: {
-        sans: [
-          'Roboto Condensed',
-          'Arial',
-          'Helvetica Neue',
-          'Helvetica',
-          'sans-serif',
-        ],
-        mono: [
-          'ui-monospace',
-          'SFMono-Regular',
-          'SF Mono',
-          'Consolas',
-          'Liberation Mono',
-          ...fontFamily.mono
-        ]
+        sans: ['Roboto Condensed', 'Arial', 'Helvetica Neue', 'Helvetica', 'sans-serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'SF Mono', 'Consolas', 'Liberation Mono', ...fontFamily.mono],
       },
       spacing: {
-        '2px': '2px'
+        '2px': '2px',
       },
       borderWidth: {
-        3: '3px'
+        3: '3px',
       },
       width: {
-        96: '24rem'
-      }
-    }
-  },
-  corePlugins: {
-    container: false
+        96: '24rem',
+      },
+    },
   },
   plugins: [
     require('./tools/tailwindcss/plugins/focus-visible'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ]
+    require('@tailwindcss/typography'),
+  ],
+  corePlugins: {
+    container: false,
+  },
+  important: true,
 }
