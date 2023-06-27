1eff8bec5efca59d4264cc104caa9beb457d1536
Oliver Davies <oliver@oliverdavies.uk>
2023-06-27 23:13:51 +0100

    docs(archive): update URLs

--- src/pages/rss/daily.xml.js
+++ src/pages/rss/daily.xml.js
@@ -21,8 +21,8 @@ export async function get() {
     }
 
     return '<br />P.S. ' + _.sample([
-      'Are you still using Drupal 7 and don’t know what’s involved to upgrade to Drupal 10? <a href="https://www.oliverdavies.dev/call">Book a Drupal 7 upgrade consultation call</a>.',
-      'Need help or want another pair of eyes on your code? Book a <a href="https://www.oliverdavies.dev/call">1-on-1 consulting call</a> or an <a href="https://www.oliverdavies.uk/pair">online pair programming session</a> with a 100% money-back guarantee.',
+      'Are you still using Drupal 7 and don’t know what’s involved to upgrade to Drupal 10? <a href="https://www.oliverdavies.uk/call">Book a Drupal 7 upgrade consultation call</a>.',
+      'Need help or want another pair of eyes on your code? Book a <a href="https://www.oliverdavies.uk/call">1-on-1 consulting call</a> or an <a href="https://www.oliverdavies.uk/pair">online pair programming session</a> with a 100% money-back guarantee.',
       'If you\'re creating a new Drupal module, try my <a href="https://github.com/opdavies/drupal-module-template">free Drupal module template</a>.',
     ]);
   };
