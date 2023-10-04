4ceabd72bb04f29dad9d8ffc2b598790217899a1
Oliver Davies <oliver@oliverdavies.uk>
2023-10-04 07:38:12 +0100

    revert: generate redirects in Astro
    This reverts commit de7aa271f2f4716b6554cf64b9afb9693809ed4a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import alpinejs from "@astrojs/alpinejs";
-import mdx from "@astrojs/mdx";
-import redirects from "./redirects.json";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,14 +16,13 @@ export default defineConfig({
         // otherwise it will be a link to a redirect URL and not the content.
         item.url = item.url.replace(/\/$/, "");
         return item;
-      },
+      }
     }),
     tailwind({
       config: {
-        applyBaseStyles: false,
-      },
+        applyBaseStyles: false
+      }
     }),
   ],
-  redirects,
-  site: "https://www.oliverdavies.uk",
+  site: "https://www.oliverdavies.uk"
 });
